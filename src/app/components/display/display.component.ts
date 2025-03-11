9469c8f48b495cd5ea8a1ad46c3b42421c4cb375
Tony Barrett <tonyb@gmx.com>
2025-03-11 20:03:16 +0000

    Final commit

--- src/app/components/display/display.component.ts
+++ src/app/components/display/display.component.ts
@@ -1,37 +1,37 @@
 import { Component } from '@angular/core';
 import { TESTDATA } from '../../testdata/products';
-import { JsonPipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-//import { Product } from '../../models/product.model';
+import { Product, ProductData } from '../../models/product.model';
 
 @Component({
   selector: 'app-display',
   standalone: true,
   imports: [
-    JsonPipe,
     FormsModule
   ],
   templateUrl: './display.component.html',
   styleUrl: './display.component.scss'
 })
 export class DisplayComponent {
-  products = TESTDATA;
-  productsToDisplay = this.products;
-
+  products: Product[] = TESTDATA; //  pulls in the data which will eventually come from an API via a service and RxJS
   pageNumber: number = 1;
-  itemsPerPage: number = 10;
+  itemsPerPage: number = 10;  //  a future enhancement would be to allow this to be change (via an <input>)
   maxPages: number = Math.floor(this.products.length / this.itemsPerPage) + 1;
 
-  constructor() {
-    console.log('products: ', this.products);
-    this.productsToDisplay = this.products.slice(0, this.itemsPerPage);
-  }
-    
+  productsToDisplay: Product[] = this.products.slice(0, this.itemsPerPage);
+
   public onPageChange(): void {
-    console.log('vdWENKKLESDFJ ', this.pageNumber)
-    console.log(this.products)
     let start: number = (this.pageNumber - 1) * this.itemsPerPage;
     let end: number = start + this.itemsPerPage;
     this.productsToDisplay = this.products.slice(start, end);
+  }
+
+  public getFeatures(rawFeatures: ProductData | null): string {
+    //  takes a object of features, removes quotes and curly braces and returns it
+    let ret: string = JSON.stringify(rawFeatures);
+    ret = ret.replace(/"+/g, ''); //  removes double quotes
+    ret = ret.replace(/[{}]/g, ''); //  removes curly braces
+    ret = ret.replace('null', '*'); //  replaces 'null' with an asterix
+    return ret;
   }
 }
--- src/app/models/product.model.ts
+++ src/app/models/product.model.ts
@@ -1,19 +1,21 @@
+export interface ProductData {
+    price?: number;
+    color?: string;
+    capacity?: string;
+    "capacity gb"?: number;
+    year?: number;
+    generation?: string;
+    "cpu model"?: string;
+    "hard disk size"?: string;
+    "strap color"?: string;
+    "case size"?: string;
+    description?: string;
+    "screen size"?: number;
+}
+
 export interface Product
 {
     id: string;
     name: string;
-    data: {
-        price?: number;
-        color: string;
-        capacity: string;
-        "capacity gb": number;
-        year: number;
-        generation: string;
-        "cpu model": string;
-        "hard-disk-size": string;
-        "strap color": string;
-        "case size": string;
-        description: string;
-        "screen size": number;
-    } | null;
+    data: ProductData | null;
 }
